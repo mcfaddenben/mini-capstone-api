e5059cf0ddfdec00027d1bca8020d84e41728348
Ben McFadden <mcfaddenben64@gmail.com>
2021-05-07 14:11:09 -0400

    adds url params for a single product return

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
     def all_products_method
         products = Product.all
-        render json: products
+        render json: products.as_json
+    end
+    def single_product
+        product = Product.find_by(name: params[:name])
+        render json: product.as_json
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/all_products_path", controller: "products", action: "all_products_method"
+  get "/single_product/:name" => "products#single_product"
 end
